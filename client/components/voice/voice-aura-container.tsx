cfa3b9f0745e0dc08e314f4de9e69d10eafaa028
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-02-27 22:32:09 +0800

    Edit: VOICE AURA CONTAINER

--- client/components/voice/voice-aura-container.tsx
+++ client/components/voice/voice-aura-container.tsx
@@ -5,3 +5,10 @@ interface VoiceAuraContainerProps {
     children: ReactNode;
 
 }
+export default function VoiceAuraContainer({ isActive, children }: VoiceAuraContainerProps) {
+    return (
+        <div className={`p-15 border-2 border-white rounded-full shadow-lg flex items-center justify-center aspect-square w-80 ${isActive ? 'animate-pulse' : ''}`}>
+            {children}
+        </div>
+    );
+}
