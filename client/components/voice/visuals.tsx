cd5bb2d927c1bea55bb8f7bdc0e206f2aecefc71
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-02-27 22:10:21 +0800

    making the the border cirle

--- client/components/voice/visuals.tsx
+++ client/components/voice/visuals.tsx
@@ -8,7 +8,7 @@ export default function Visuals() {
     return (
         <>
         <div className="flex flex-col items-center justify-center min-h-screen  gap-8">
-        <div className="p-8 bg-white rounded-2xl shadow-lg flex flex-col items-center gap-6 w-80">
+        <div className="p-25 bg-white rounded-full shadow-lg flex items-center justify-center aspect-square w-80">
             <VoiceVisualizer isSpeaking={isSpeaking} />
         </div>
         <button 
