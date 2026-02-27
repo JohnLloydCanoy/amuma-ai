760fcbd7c82f245335d2822dca354f9225219c06
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-02-27 18:41:17 +0800

    importing the component and assisting to the function

--- client/app/Home/page.tsx
+++ client/app/Home/page.tsx
@@ -1,9 +1,14 @@
 "use client";
+import React, { useState } from 'react';
+import VoiceVisualizer from '../../components/voice-visualizer';
+
 export default function HomePage() {
+    const [isSpeaking, setIsSpeaking] = useState(false);
+
     return (
         <>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        
+        <VoiceVisualizer isSpeaking={true} />   
         </>
     );
 }
