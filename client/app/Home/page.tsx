adcdc29e365ddeedbda727cd7dbd54ddd2f3a89f
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-02-27 21:57:06 +0800

    cleaning teh HomePage file

--- client/app/Home/page.tsx
+++ client/app/Home/page.tsx
@@ -6,7 +6,6 @@ export default function HomePage() {
 
     return (
         <>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
         <div className="flex flex-col items-center justify-center min-h-screen  gap-8">
 
         <div className="p-8 bg-white rounded-2xl shadow-lg flex flex-col items-center gap-6 w-80">
