d97a13244df11fa7eb112430fecf4b8d5f28ec45
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-02-27 22:20:36 +0800

    decreasing the paddingl

--- client/components/voice/visuals.tsx
+++ client/components/voice/visuals.tsx
@@ -8,7 +8,7 @@ export default function Visuals() {
     return (
         <>
         <div className="flex flex-col items-center justify-center min-h-screen  gap-8">
-        <div className="p-25 bg-white rounded-full shadow-lg flex items-center justify-center aspect-square w-80">
+        <div className="p-15 bg-white rounded-full shadow-lg flex items-center justify-center aspect-square w-80">
             <VoiceVisualizer isSpeaking={isSpeaking} />
         </div>
         <button 
