ac5dca2b6923f01fc6095252d938a65126164a3b
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-02-27 21:57:23 +0800

    cleaning teh HomePage file

--- client/app/Home/page.tsx
+++ client/app/Home/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
     return (
         <>
         
-<div className="p-8 bg-white rounded-2xl shadow-lg flex flex-col items-center gap-6 w-80">
+
             <h2 className="text-xl font-semibold text-slate-700">Amuma AI Interface</h2>
 
             <Visuals isSpeaking={isSpeaking} />
