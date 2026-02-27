356d14cfddf9f7c44408d6be9135edbee0718032
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-02-27 21:58:03 +0800

    cleaning teh HomePage file

--- client/app/Home/page.tsx
+++ client/app/Home/page.tsx
@@ -15,8 +15,7 @@ export default function HomePage() {
             onClick={() => setIsSpeaking(!isSpeaking)}
             className="px-6 py-3 bg-slate-600 text-white rounded-full font-medium hover:bg-slate-700 transition-colors shadow-md"
         >
-            {isSpeaking ? "Stop AI Speech" : "Simulate AI Speaking"}
-        </button>
+
 
             </>
     );
