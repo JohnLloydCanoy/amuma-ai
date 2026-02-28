9333ac3b47c1bdc9956617e5744e963684b770a0
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-01 00:51:58 +0800

    iimporting from google import genai

--- server/main.py
+++ server/main.py
@@ -3,3 +3,5 @@ import asyncio
 from dotenv import load_dotenv
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
+from google import genai
+
