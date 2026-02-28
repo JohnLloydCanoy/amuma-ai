cd8f09ab32300bb4da9d51185688fbfbfdc81891
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-01 01:27:34 +0800

    Update: Voice Session FIle

--- client/components/voice/voice-session.tsx
+++ client/components/voice/voice-session.tsx
@@ -3,3 +3,4 @@ import React, { useState, useRef } from "react";
 import VoiceVisualizer from "./voice-visualizer";
 
 export default function VoiceSession() {
+    const [isSpeaking, setIsSpeaking] = useState(false);
